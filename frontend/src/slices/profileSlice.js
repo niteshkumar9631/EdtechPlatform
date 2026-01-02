9425aa1be9088f91f85c5e8a4f1aaada479e5692
jhayuvraj2004@gmail.com <jhayuvraj2004@gmail.com>
2026-01-02 23:28:26 +0530

    modified:   frontend/src/slices/profileSlice.js

--- frontend/src/slices/profileSlice.js
+++ frontend/src/slices/profileSlice.js
@@ -6,24 +6,24 @@ const initialState = {
 };
 
 // Problem occured and solved
-// initially i mark user data as nulll
+// initially i mark user data as null
 // as i refresh the page user becomes null , so login / signup buttons are not visible - In case of user not logged
 // case - User logged but as i refresh , dashboard dropdown becomes invisible
 // solution - try getting value from localStorage otherwise mark it as null
 
 
-const profileSlice = createSlice({
-    name: "profile",
-    initialState: initialState,
-    reducers: {
-        setUser(state, value) {
-            state.user = value.payload;
-        },
-        setLoading(state, value) {
-            state.loading = value.payload
-        }
-    },
-});
+// const profileSlice = createSlice({
+//     name: "profile",
+//     initialState: initialState,
+//     reducers: {
+//         setUser(state, value) {
+//             state.user = value.payload;
+//         },
+//         setLoading(state, value) {
+//             state.loading = value.payload
+//         }
+//     },
+// });
 
 export const { setUser, setLoading } = profileSlice.actions;
 export default profileSlice.reducer;
