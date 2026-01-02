7753d3124995c97c6cecb51ae4b2216b1b134ac1
jhayuvraj2004@gmail.com <jhayuvraj2004@gmail.com>
2026-01-02 23:24:19 +0530

    modified:   frontend/src/slices/viewCourseSlice.js

--- frontend/src/slices/viewCourseSlice.js
+++ frontend/src/slices/viewCourseSlice.js
@@ -29,12 +29,12 @@ const viewCourseSlice = createSlice({
   },
 })
 
-// export const {
-//   setCourseSectionData,
-//   setEntireCourseData,
-//   setTotalNoOfLectures,
-//   setCompletedLectures,
-//   updateCompletedLectures,
-// } = viewCourseSlice.actions
+export const {
+  setCourseSectionData,
+  setEntireCourseData,
+  setTotalNoOfLectures,
+  setCompletedLectures,
+  updateCompletedLectures,
+} = viewCourseSlice.actions
 
 export default viewCourseSlice.reducer
