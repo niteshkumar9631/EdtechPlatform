76cc82409ae592959efc098eafe6212a42a4a32d
jhayuvraj2004@gmail.com <jhayuvraj2004@gmail.com>
2026-01-02 22:46:10 +0530

    modified:   frontend/src/components/core/ViewCourse/VideoDetailsSidebar.jsx

--- frontend/src/components/core/ViewCourse/VideoDetailsSidebar.jsx
+++ frontend/src/components/core/ViewCourse/VideoDetailsSidebar.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react
+import { useEffect, useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { useLocation, useNavigate, useParams } from "react-router-dom"
 
